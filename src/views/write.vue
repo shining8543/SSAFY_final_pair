3615d85e7055b50b45e2dd3402b3e3698734453c
김경원 <shining8543@naver.com>
2021-05-18 18:16:44 +0900

    write form

---
+++ src/views/write.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h4>게시글 작성</h4>
+    <b-container>
+      <b-form-group id="input-name" label="제목" label-for="input-title">
+        <b-form-input
+          id="input-title"
+          type="text"
+          v-model="board.btitle"
+          placeholder="제목을 입력하세요"
+          required
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group id="input-name" label="내용" label-for="input-content">
+        <b-form-textarea
+          id="input-content"
+          type="text"
+          v-model="board.bcontent"
+          placeholder="내용을 입력하세요"
+          rows="10"
+          max-rows="10"
+          required
+        ></b-form-textarea>
+      </b-form-group>
+
+      <b-button @click="add">등록하기</b-button>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      board: {
+        bnum: "",
+        bwriter: "",
+        btitle: "",
+        bcontent: "",
+        bregdate: "",
+      },
+    };
+  },
+  methods: {
+    add() {
+      this.$store.dispatch("postBoard", this.board).then(() => {});
+    },
+  },
+};
+</script>
+
+<style>
+</style>
