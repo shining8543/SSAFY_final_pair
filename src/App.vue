3886c47434d2f9c51b16c8b32ac2172cfd3204ef
김경원 <shining8543@naver.com>
2021-05-18 14:58:56 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">게시판</router-link>
+      <router-link to="/board">게시판</router-link>
     </div>
     <router-view />
   </div>
